e39882250f84b874cd0ee1eac045a2ecbf683c39
ankikiluo <luo.an@northeastern.edu>
2022-03-30 18:30:31 -0400

    finish exercise 6

--- controllers/tuits-controller.js
+++ controllers/tuits-controller.js
@@ -28,7 +28,6 @@ const findAllTuits = (req, res) => {
 const createTuit = (req, res) => {
     const newTuit = req.body;
     newTuit._id = (new Date()).getTime()+'';
-    newTuit.likes = 0;
     tuits.push(newTuit);
     res.json(newTuit);
 }
