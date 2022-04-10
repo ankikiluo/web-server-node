3e4afa1f28bd7775d9e7764fb62ee20ea136e037
ankikiluo <luo.an@northeastern.edu>
2022-04-10 11:05:23 -0400

    temp of full copy for a8

--- db/tuits/tuits-dao.js
+++ db/tuits/tuits-dao.js
@@ -1,10 +1,10 @@
 // const model = require('./model');
 import model from "./tuits-model.js";
 const findAllTuits = () => model.find();
-// const deleteMovie = (id) => model.removeOne({_id: id});
 const deleteTuit = (id) => model.deleteOne({_id: id});
 const createTuit = (tuit) => model.create(tuit);
 // const findTuitById = (id) => model.findById(id);
 const updateTuit = (id, tuit) => model.updateOne({_id: id}, {$set: tuit});
 
 export default {findAllTuits, deleteTuit, createTuit, updateTuit};
+// module.exports = {findAllTuits: findAllTuits(), deleteTuit, createTuit, updateTuit}
